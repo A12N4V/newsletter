b09191f888ba86de98e8bc18eac4d908f5c161b6
Arnav Sharma <aaarnavsssharma@gmail.com>
2022-01-04 12:17:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //jshint eversion: 6
 require('dotenv').config();
-const port = 8080
+const port = 8080;
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
@@ -26,19 +26,19 @@ app.post("/", function(req, res) {
         }
       }
     ]
-  }
-  var jsonData = JSON.stringify(data)
+  };
+  var jsonData = JSON.stringify(data);
   const url = "https://us6.api.mailchimp.com/3.0/lists/0054498f61";
   const options = {
     method: "POST",
     auth: process.env.AUTH
-  }
+  };
   const request = https.request(url, options, function(response) {
     if (response.statusCode == 200) {
       res.sendFile(__dirname + "/success.html")
     } else {
        res.sendFile(__dirname + "/failure.html")
-    }
+    };
     response.on("data", function(data) {
       console.log(JSON.parse(data));
     });
